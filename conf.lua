4d378436aeff2cd8e32e94eb151b61dbabe18686
gauthier <gauthier@appsvelocity.com>
2022-06-18 12:14:53 +0200

    start

--- conf.lua
+++ conf.lua
@@ -1,3 +1,5 @@
+-- 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 got token
+
 function love.conf(t)
     t.console = false
 end
--- system_collision.lua
+++ system_collision.lua
@@ -9,13 +9,7 @@ function S_Collision.__filter_colls(e,o)
     local c_b=e.c_b
     if Xtype.is(o, E_Tile) then
         local c_tile = o.c_tile
-        
-        if c_tile:has_only_props(Tl.Prop.Ladder) then
-            return 'cross'
-        end
-        if c_tile:has_prop(Tl.Prop.Platform) then
-            return 'cross'
-        end
+
         if c_tile:has_prop(Tl.Prop.Ground) then
             return 'slide'
         end
@@ -47,78 +41,20 @@ function S_Collision:process(e, dt)
             local tl=o
             local c_tile=o.c_tile
 
-            --== hit climbable landder
-            -- if c_tile:has_prop(Tl.Prop.Ladder) then
-            --     c_b.hit_ladder=coll
-            -- end
-
-            --== hit ceil
-            if c_tile:has_prop(Tl.Prop.Ceil) and coll.normal.y == 1 then
-                c_b.hit_ceil=coll
-            end
-
-            --== hit climbable wall corner
-            -- if c_tile:has_prop(Tl.Prop.Wall) and coll.normal.x ~= 0 and c_b.vy > 0 then
-            --     if GAME.map:is_tl_corner_wall(c_tile.index.x, c_tile.index.y, -coll.normal.x) then
-            --         if c_b:top() > o.c_b:top() and c_b:top() < o.c_b:top()+6 then
-            --             c_b.hit_corner=coll
-            --         end
-            --     end
-            -- end
-
             
-            
-
-            --== platform
-            if c_tile:has_prop(Tl.Prop.Platform) then
-
-                --== hit ground
-                if coll.normal.y == -1 and c_b:bot() < tl.c_b:top()+4 then
-                    c_b.hit_ground=coll
-                end
-
-                if c_sm and not  Xtype.is(c_sm:get(), C_HangPlatformState) then
-                    --== check grabbable
-                    if c_b.platform_land_enabled and c_b.vy > 0 and goal_y > tl.c_b:top() and goal_y < tl.c_b:top()+4 then
-                        c_b.can_grab_platform=coll
-                      --print('can grab platform', c_b:top(), goal_y, tl.c_b:top())
-                    end
-                end
-
-            --== hit ground
-            elseif c_tile:has_prop(Tl.Prop.Ground) then
-                if coll.normal.y == -1 then
-                    c_b.hit_ground=coll
-                end
-            end
-           
         end
     end
 
-    --== handle special collisions
-    if c_b.hit_ground then --== for landing on platform
-        local tl = c_b.hit_ground.other
-      --print('landing on platform')
-        goal_y=tl.c_b.y-c_b.h
-    end
-
-    --== grabing platform
-    if c_b.can_grab_platform then
-        local tl = c_b.can_grab_platform.other
-        goal_y=tl.c_b.y
-        c_b.is_platform_hanging=true
-    end
-
-    c_sm = e.c_state_machine
-    if c_sm then
-        local state = c_sm:get()
-        if Xtype.is(state, C_HangPlatformState) and state.is_climbing then
-            local coll = state.args.platform_coll
+    -- c_sm = e.c_state_machine
+    -- if c_sm then
+    --     local state = c_sm:get()
+    --     if Xtype.is(state, C_HangPlatformState) and state.is_climbing then
+    --         local coll = state.args.platform_coll
        
-            goal_y = coll.other.c_b:top()-c_b.h
+    --         goal_y = coll.other.c_b:top()-c_b.h
        
-          --print('=>',state.args.platform_coll.other)
-        end
+    --       --print('=>',state.args.platform_coll.other)
+    --     end
     end
 
     --== is still on ground ?
